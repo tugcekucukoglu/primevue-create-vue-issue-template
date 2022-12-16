fc3808286488d6b9e5872b768b51036419a9409d
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2022-12-16 14:48:13 +0300

    logo added

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 </script>
 
 <template>
+    <img src="./assets/primevue-logo.png" alt="PrimeVue Logo" />
     <h2>PrimeVue Issue Template</h2>
     <p>Please create a test case and attach the link of the codesandbox to your github issue report.</p>
     <Button type="button" label="PrimeVue Button"></Button>
